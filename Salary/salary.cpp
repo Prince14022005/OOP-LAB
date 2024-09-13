41094ea638666c4a043d1b7e48f23e407c26b763
Prince14022005 <princegoyal1402@gmail.com>
2024-09-13 12:18:08 +0530

    Getter and Setter program of salary

---
+++ Salary/salary.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Employee
+{
+private:
+    int salary;
+
+public:
+    void SetSalary(int s)
+    {
+        salary = s;
+    }
+    int getSalary()
+    {
+        return (salary);
+    }
+};
+int main()
+{
+    Employee myobj;
+    myobj.SetSalary(150000);
+    cout << myobj.getSalary() << endl;
+    return 0;
+}
